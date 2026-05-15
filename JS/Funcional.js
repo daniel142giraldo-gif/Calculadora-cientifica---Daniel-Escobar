9b0b21c5593d09ab05f6cdc48dc8793aaf239717
Daniel Escobar <daniel142giraldo@gmail.com>
2026-05-15 13:25:14 -0500

    Funciones

--- JS/Funcional.js
+++ JS/Funcional.js
@@ -1,3 +1,46 @@
 let p = {
-    teclas = document.querySelectorAll("#calculadora ul li")
+    teclas = document.querySelectorAll("#calculadora ul li"),
+    accion = null,
+    digito = null,
+    operaciones = document.querySelector("#operaciones"),
+    cantisigno = 0,
+    cantdecimal = false,
+    resultado = false
+}
+
+let m = {
+    inicio:function()
+    {
+        for(let i = 0; i < p.teclas.length; i++)
+        {
+            p.teclas[i].addEventListener("click", m.oprimirtecla)
+        }
+    },
+    oprimirtecla:function(tecla)
+    {
+        p.accion = tecla.target.getAttribute("class");
+        m.calculadora(p.accion);
+    },
+    calculadora:function()
+    {
+        switch(accion)
+        {
+            case "numero":
+                console.log("numero");
+
+            break;
+            case "signo":
+                console.log("signo");
+
+            break;
+            case "decimal":
+                console.log("decimal");
+
+            break;
+            case "igual":
+                console.log("igual");
+
+            break;
+        }
+    }
 }
